8246b16f3055503392e540c3b4e9544a63f9f588
arqamtahir <muhammadarqam920@gmail.com>
2022-08-01 03:43:01 +0500

    stock controller and action to link form request

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
  root to: 'wlcm#index' 
    get 'wlcm/index'
+get 'my_portfolio' , to: 'users#my_portfolio'
+get 'search_stocks' , to: 'stocks#search'
+
 end
